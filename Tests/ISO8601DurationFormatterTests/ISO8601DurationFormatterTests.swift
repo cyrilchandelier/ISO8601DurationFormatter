3991eb44985dd25a3347249afb92351b311a2643
Cyril Chandelier <cyril.chandelier@gmail.com>
2021-08-01 12:55:29 -0700

    Initial Commit

---
+++ Tests/ISO8601DurationFormatterTests/ISO8601DurationFormatterTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import ISO8601DurationFormatter
+
+    final class ISO8601DurationFormatterTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(ISO8601DurationFormatter().text, "Hello, World!")
+        }
+    }
